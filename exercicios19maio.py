12b052937ce94b65febf15ccbae10f7422718add
nkribeiro27 <163589957+nkribeiro27@users.noreply.github.com>
2025-05-19 14:48:06 -0300

    Create exercicios19maio.py

---
+++ exercicios19maio.py
@@ -0,0 +1,144 @@
+#num = int(input('Digite um numero: '))
+#num2 = int(input('Digite um numero: '))
+#num3 = int(input('Digite um numero: '))
+#num4 = int(input('Digite um numero: '))
+#num5 = int(input('Digite um numero: '))
+
+#soma = num + num2 + num3 + num4 + num5
+
+#print(soma)
+
+######################################
+
+#num1 = int(input('Digite um numero: '))
+#num2 = int(input('Digite um numero: '))
+#num3 = int(input('Digite um numero: '))
+#num4 = int(input('Digite um numero: '))
+
+#if num1 > num2 and num1 > num3 and num1 > num4:
+#    print(num1)
+
+#elif num2 > num1 and num2 > num3 and num1 > num4:
+#    print(num2)    
+
+#elif num3 > num1 and num3 > num2 and num3 > num4:
+#    print(num3)
+
+#elif num4 > num1 and num4 > num2 and num4 > num3:
+#    print(num4)    
+
+######################################
+
+#palavra = input('Digite uma palavra: ')
+#vogais = "aeiouAEIOU"
+#numvog = 0
+
+#for letra in palavra:
+#    if letra in vogais:
+#        numvog +=1
+
+#print(numvog)
+
+######################################        
+
+#num = int(input('Digite o primeio nùmero:'))
+#num2 = int(input('Digite o segundo número:'))
+#num3 = int(input('Digite o terceiro número:'))
+#num4 = int(input('Digite o quarto número:'))
+#num5 = int(input('Digite o quinto número:'))
+#num6 = int(input('Digite o sexto número: '))
+
+#if num % 2 == 0:
+ #   print('1°par')
+    
+#else:
+  #  print('1°impar')    
+
+#if num2 % 2 == 0:
+ #   print('2°par')
+
+#else:
+ #   print('2°impar')
+
+#if num3 % 2 == 0:
+ #   print('3°par')
+
+#else:
+ #   print('3°impar')    
+
+#if num4 % 2 == 0:
+ #   print('4°par')
+
+#else:
+ #   print('4°impar')    
+
+#if num5 % 2 == 0:
+ #   print('5°par')
+
+#else:
+ #   print('5°impar')    
+
+#if num6 % 2 == 0:
+ #   print('6°par')
+
+#else:
+   #print('6°impar')
+
+######################################
+
+#nota = float(input('Digite a nota da prova: '))
+#nota2 = float(input('Digite a nota da prova: '))
+#nota3 = float(input('Digite a nota da prova: '))
+#nota4 = float(input('Digite a nota da prova: '))
+
+#media = (nota + nota2 + nota3 + nota4) / 4
+
+#print(media)
+
+######################################
+
+#num = int(input('Digite um numero: '))
+#vzs = 1
+
+#while vzs <= 10:
+ #   print(num , 'x' , vzs , '=' , num * vzs)
+  #  vzs +=1
+
+######################################
+
+#num = int(input('Digite um numero: '))
+#n1 = 0
+
+#while n1 <= num:
+   # print(n1)
+    #n1 += 1 
+
+######################################
+
+#palavra = input('Digite uma palvra: ')
+#rvpalvra = palavra[::-1]
+#print(rvpalvra)
+
+######################################
+
+#num = int(input('Digite um numero: '))
+
+#if num % 3 == 0 :
+   #print('multiplo de 3')   
+#else:
+   #print('não é múltiplo de 3') 
+
+
+#####################################
+#nome = input('Digite um nome: ')
+#nome2 = input('Digite um nome: ')
+#nome3 = input('Digite um nome: ')
+
+#nome_a = sorted(nome)
+#nome_b = sorted(nome2)
+#nome_c = sorted(nome3)
+
+#print(nome_a)
+#print(nome_b)
+#print(nome_c)
+####################################
